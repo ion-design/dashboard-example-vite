bc4e629fdf01154f89a725c4890720913c137fc4
Samraaj Bath <samraaj100@gmail.com>
2024-11-06 08:45:56 -0800

    Create buy page
    modified: src/App.tsx
    modified: src/Generation.tsx
    modified: src/components/ion/Sidebar.tsx
    modified: src/components/ion/Avatar.tsx
    modified: src/components/ion/Navigation.tsx
    modified: src/components/ion/Tabs.tsx
    modified: src/components/ion/Badge.tsx
    modified: src/components/ion/Button.tsx
    modified: src/components/ion/Input.tsx
    modified: src/components/ion/Label.tsx
    modified: src/components/ion/Hint.tsx
    modified: src/components/ion/Popover.tsx
    modified: src/components/ion/Datepicker.tsx
    modified: src/components/ion/TokenMetric.tsx
    modified: src/components/ion/Calendar.tsx
    modified: src/components/ion/SideNavigation.tsx
    added: src/components/ion/BuyExample.tsx
    added: src/components/ion/BuyCard.tsx

--- src/components/ion/Hint.tsx
+++ src/components/ion/Hint.tsx
@@ -1,6 +1,6 @@
-// Generated with Ion on 11/5/2024, 12:43:53 PM
+// Generated with Ion on 11/6/2024, 8:39:48 AM
 // Figma Link: https://www.figma.com/design/BsgE00bYWTmwm4RY0WmYN6?node-id=423:1500
-// ion/Hint: Generated with Ion on 11/5/2024, 12:43:47 PM
+// ion/Hint: Generated with Ion on 11/6/2024, 8:39:43 AM
 import { Info } from "@phosphor-icons/react/dist/ssr";
 import clsx from "clsx";
 
