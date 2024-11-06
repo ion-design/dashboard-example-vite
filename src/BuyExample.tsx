8fa14bfdb8103d5a1ba1647cf73b61e58899dd8f
Samraaj Bath <samraaj100@gmail.com>
2024-11-05 17:15:29 -0800

    Implement Buy screen
    modified: src/Generation.tsx
    added: src/BuyExample.tsx
    modified: src/components/ion/Navigation.tsx
    modified: src/components/ion/SideNavigation.tsx
    added: src/components/ion/BuyExample.tsx
    added: src/components/ion/BuyCard.tsx

---
+++ src/BuyExample.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+    function BuyExample() {
+      return (
+        <div>
+          <h1>Buy Page</h1>
+          <p>This is the Buy page content.</p>
+        </div>
+      );
+    }
+
+    export default BuyExample;
